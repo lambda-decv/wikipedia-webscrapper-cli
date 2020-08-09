c8f5d0225478826b30007f52e1b5b859ffe81428
TEASTERR <lexou.bzd@gmail.com>
2020-08-10 00:08:41 +0200

    first version

--- app.js
+++ app.js
@@ -6,15 +6,44 @@ const readline = require('readline').createInterface({
 });
 
 readline.question(`Give me a link of wikipedia page `, (link) => {
-    const wikipediaLink = 'fr.wikipedia.org'.split(/[^/]*$/g)    
+    const wikipediaBaseLink = 'wikipedia.org';
+    const linkWithoutPrefix = link.replace(/(^(.*?)\.)/, "");
+    const linkWithoutSuffix = linkWithoutPrefix.replace(/(\/(.*)$)/, "");
+    const baseLink = linkWithoutSuffix;
+    if (baseLink == wikipediaBaseLink){
+        (async () => {
+            const browser = await puppeteer.launch()
+            const page = await browser.newPage()
+            await page.goto(link)
+
+            const titleOfPage = await page.$(".firstHeading")
+            const textTitle = await page.evaluate(titleOfPage => titleOfPage.textContent, titleOfPage);
+            console.log(textTitle)
+
+            const summary = await page.$(".toc");
+            const textSummary = await page.evaluate(summary => summary.textContent, summary);
+            console.log(textSummary)
+            
+            await browser.close()
+        })();
+    }else{
+        (async () => {
+            const browser = await puppeteer.launch()
+            const page = await browser.newPage()
+            await page.goto("https://fr.wikipedia.org/wiki/Sp%C3%A9cial:RandomRootpage")
+
+            console.log("Le lien indiqué n'est pas valide, voici donc le lien d'une page aléatoire")
+
+            const titleOfPage = await page.$(".firstHeading")
+            const textTitle = await page.evaluate(titleOfPage => titleOfPage.textContent, titleOfPage);
+            console.log(textTitle)
+            
+            const summary = await page.$(".toc");
+            const textSummary = await page.evaluate(summary => summary.textContent, summary);
+            console.log(textSummary)
+
+            await browser.close()
+        })();
+    }
     readline.close()
 });
-(async () => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto(link)
-    const element = await page.$(".toc");
-    const text = await page.evaluate(element => element.textContent, element);
-    console.log(text)
-    await browser.close()
-})();
